4973af9d2245f5680f285c6eb91063cadf6890d0
Jan H. Jensen <jhjensen@chem.ku.dk>
2017-03-08 13:26:35 +0100

    Create lowpka4.py

---
+++ lowpka4.py
@@ -0,0 +1,60 @@
+import sys
+import pickle
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+from collections import defaultdict
+#import name2atom as n2a
+
+filename = sys.argv[1]
+file = open(filename, "r")
+energies = defaultdict(list)
+log_files = defaultdict(list)
+ions = defaultdict(list)
+min_e_ion = defaultdict(list)
+min_e_log = defaultdict(list)
+names = []
+sorted_e = []
+
+n2aname = filename.split("_")[0] + ".p"
+name2atom = pickle.load( open( n2aname, "rb"))
+#print name2atom
+
+e_cutoff = 1.0
+
+
+#print allsmiles
+for line in file:
+    words = line.split()
+    log_file  = words[0][:-1]
+    name = words[0].split("_")[0][:-1]
+    if words[1] == "CURRENT":
+        energy = float(words[-1])
+    else:
+        energy = float(words[6])
+    if name not in names:
+        names.append(name)
+    ion = log_file.split("-")[0]
+    if ion not in ions[name]:
+        ions[name].append(ion)
+    log_files[ion].append(log_file)
+    energies[ion].append(energy)
+
+
+for name in names:
+    for ion in ions[name]:
+        min_e_ion[name].append(min(energies[ion]))
+        where_in_list = energies[ion].index(min(energies[ion]))
+        min_e_log[name].append(log_files[ion][where_in_list])
+    e_min = min(min_e_ion[name])
+    sorted_e = sorted(min_e_ion[name])
+    sorted_e_index = sorted(range(len(min_e_ion[name])), key=lambda k: min_e_ion[name][k])
+    atoms = ""
+    logfiles = ""
+    for i,e in enumerate(sorted_e):
+        if e - e_min < e_cutoff:
+            j = sorted_e_index[i]
+            atoms += str(name2atom[ions[name][j]])+","
+            logfiles += str(min_e_log[name][j])+","
+
+    print name, atoms, logfiles
